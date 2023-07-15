5fc4f2baf72a4d1741287f111cfa76519b488aea
Lkoech <lydiakip1@gmail.com>
2023-07-15 22:58:47 +0300

    Minor improvments in app

--- components/home/nearby/Nearbyjobs.jsx
+++ components/home/nearby/Nearbyjobs.jsx
@@ -1,47 +1,45 @@
-import { 
-  View, 
-  Text ,
-  TouchableOpacity,
-  ActivityIndicator
-} from 'react-native'
+import React from "react";
 import { useRouter } from "expo-router";
+import { View, Text, TouchableOpacity, ActivityIndicator } from "react-native";
+
 import styles from "./nearbyjobs.style";
 import { COLORS } from "../../../constants";
 import NearbyJobCard from "../../common/cards/nearby/NearbyJobCard";
 import useFetch from "../../../hook/useFetch";
 
-const Nearbyearbyjobs = () => {
+const Nearbyjobs = () => {
   const router = useRouter();
-  const {data, isLoading, error} = useFetch("search", {
-      query:"java developer",
-      num_pages:1
-    });
+  const { data, isLoading, error } = useFetch("search", {
+    query: "React Native developer",
+    num_pages: "1",
+  });
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
         <Text style={styles.headerTitle}>Nearby jobs</Text>
         <TouchableOpacity>
-          <Text style={styles.headerBtn}>View all</Text>
+          <Text style={styles.headerBtn}>Show all</Text>
         </TouchableOpacity>
-      </View> 
+      </View>
+
       <View style={styles.cardsContainer}>
         {isLoading ? (
-          <ActivityIndicator size="large" color={COLORS.primary} />
-        ): error ? (
-          <Text >Something went wrong</Text>
-        ):(
+          <ActivityIndicator size='large' color={COLORS.primary} />
+        ) : error ? (
+          <Text>Something went wrong</Text>
+        ) : (
           data?.map((job) => (
             <NearbyJobCard
-            job = {job}
-            key = {"nearby-job-${job?.job_id}"}
-            handleNavigate = {()=> router.push('/job-details/${job.job_id}')}
+              job={job}
+              key={`nearby-job-${job.job_id}`}
+              handleNavigate={() => router.push(`/job-details/${job.job_id}`)}
             />
-        ))
+          ))
         )}
-      </View>  
+      </View>
     </View>
-  )
-}
+  );
+};
 
-
-export default Nearbyjobs
+export default Nearbyjobs;
--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -1,12 +1,13 @@
-import { useState } from 'react'
-import { 
-  View, 
-  Text ,
+import { useState } from "react";
+import { useRouter } from "expo-router";
+import {
+  View,
+  Text,
   TouchableOpacity,
   FlatList,
-  ActivityIndicator
-} from 'react-native'
-import { useRouter } from "expo-router";
+  ActivityIndicator,
+} from "react-native";
+
 import styles from "./popularjobs.style";
 import { COLORS, SIZES } from "../../../constants";
 import PopularJobCard from "../../common/cards/popular/PopularJobCard";
@@ -14,40 +15,47 @@ import useFetch from "../../../hook/useFetch";
 
 const Popularjobs = () => {
   const router = useRouter();
-  const {data, isLoading, error} = useFetch("search", {
-      query:"java developer",
-      num_pages:1
-    });
+  const { data, isLoading, error } = useFetch("search", {
+    query: "React developer",
+    num_pages: "1",
+  });
+  const [selectedJob, setSelectedJob] = useState();
+  const handleCardPress = (item) => {
+    router.push(`/job-details/${item.job_id}`);
+    setSelectedJob(item.job_id);
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
         <Text style={styles.headerTitle}>Popular jobs</Text>
         <TouchableOpacity>
-          <Text style={styles.headerBtn}>View all</Text>
+          <Text style={styles.headerBtn}>Show all</Text>
         </TouchableOpacity>
-      </View> 
+      </View>
       <View style={styles.cardsContainer}>
         {isLoading ? (
-          <ActivityIndicator size="large" color={COLORS.primary} />
-        ): error ? (
-          <Text >Something went wrong</Text>
-        ):(
+          <ActivityIndicator size='large' color={COLORS.primary} />
+        ) : error ? (
+          <Text>Something went wrong</Text>
+        ) : (
           <FlatList
             data={data}
-            renderItem={({item}) => (
-              <PopularJobCard 
+            renderItem={({ item }) => (
+              <PopularJobCard
                 item={item}
+                selectedJob={selectedJob}
+                handleCardPress={handleCardPress}
               />
             )}
-            keyExtractor={(item) => item?.job_id}
-            contentContainerStyle={{columnGap: SIZES.medium}}
+            keyExtractor={(item) => item.job_id}
+            contentContainerStyle={{ columnGap: SIZES.medium }}
             horizontal
           />
-        )
-        }
-      </View>  
+        )}
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default Popularjobs
+export default Popularjobs;
